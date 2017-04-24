91e0bbe8e9e665e2482c5386495642d37483d214
Yang Wang <deepcontrolr@gmail.com>
2017-04-24 09:57:51 -0700

    Initial Commit

---
+++ iKid/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iKid
+//
+//  Created by William on 4/24/17.
+//  Copyright © 2017 Yang Wang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
